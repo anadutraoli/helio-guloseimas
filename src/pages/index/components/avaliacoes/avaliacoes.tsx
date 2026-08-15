c0d4fd6e6920d5896fd1006607565777af5d20d2
Ana Maria Dutra de Oliveira <canaldaanacontato@gmail.com>
2026-08-15 01:11:10 -0300

    feat: adicionando componente avaliacoes

---
+++ src/pages/index/components/avaliacoes/avaliacoes.tsx
@@ -0,0 +1,51 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./avaliacoes.css";
+import { useEffect, useState } from "react";
+import { carregarDados } from "../../../../services/api";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+type Avaliacoes = {
+  id: number;
+  nome: string;
+  text: string;
+};
+
+export default function Avaliacoes() {
+  const [avaliacoes, setAvaliacoes] = useState<Avaliacoes[]>([]);
+
+  useEffect(() => {
+    carregarDados("avaliacoes")
+      .then((dados) => {
+        setAvaliacoes(dados);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <article className="avaliacoes-bg">
+        <div className="avaliacoes container">
+          <h1>O que os clientes estão achando?</h1>
+          <Swiper
+            spaceBetween={30}
+            autoplay={{ delay: 5000, disableOnInteraction: true }}
+            loop={true}
+            modules={[Autoplay]}
+          >
+            {avaliacoes.map((avaliacao) => (
+              <SwiperSlide key={avaliacao.id}>
+                <div className="content-avaliacoes">
+                  <h2>{avaliacao.nome}</h2>
+                  <p>{avaliacao.text}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </article>
+    </>
+  );
+}
